3aa062b32612fc64fe9794e2bdd29abed5d8c15b
SyafiraRosa <firarosa966@gmail.com>
2021-09-26 03:00:25 +0700

    MatchThree

---
+++ Assets/Scripts/UIScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScore : MonoBehaviour
+{
+    public Text highSore;
+    public Text currentSore;
+
+    private void Update()
+    {
+        highSore.text = ScoreManager.Instance.HighScore.ToString();
+        currentSore.text = ScoreManager.Instance.CurrentScore.ToString();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
